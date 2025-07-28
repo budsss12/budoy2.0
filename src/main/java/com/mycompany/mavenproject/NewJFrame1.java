f91c7f253d5957d30d3c51878e7715cc7cacfa3a
CL3-PC29 <CL3-PC29@DESKTOP-ROLBJ8U>
2025-07-28 16:08:34 +0800

    sample.program act2c

--- src/main/java/com/mycompany/mavenproject/NewJFrame1.java
+++ src/main/java/com/mycompany/mavenproject/NewJFrame1.java
@@ -31,11 +31,11 @@ public class NewJFrame1 extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtnum1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
         txtnum2 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         txtresult1 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -47,14 +47,6 @@ public class NewJFrame1 extends javax.swing.JFrame {
         jLabel2.setName(""); // NOI18N
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 160, 130, 20));
         getContentPane().add(txtnum1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 30, 190, -1));
-
-        jButton1.setText("Add");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 100, -1, -1));
         getContentPane().add(txtnum2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 70, 190, -1));
 
         jLabel3.setText("Number 1");
@@ -69,27 +61,26 @@ public class NewJFrame1 extends javax.swing.JFrame {
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 190, 130, -1));
         getContentPane().add(txtresult1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 130, 130, -1));
 
+        jButton3.setText("Add");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 100, -1, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-
-        int num1 = Integer.parseInt(txtnum1.getText());
-        int num2 = Integer.parseInt(txtnum2.getText());
-
-        int sum = num1 + num2;
-
-        jLabel2.setText("the sum is:: "+sum);
-
-        JOptionPane.showMessageDialog(null, "the sum is: " + sum, "ACT 2C", JOptionPane.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         this.dispose();
         new NewJFrame2().show(true);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -127,8 +118,8 @@ public class NewJFrame1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
